3e8530aee5ece0e67c60c9fc7906ee88343918d0
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-06 14:12:25 -0700

    Completes EvenOrOdd

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -9,3 +9,9 @@ function divisibleBy(numbers, divisor){
   return dividends
 }
 
+//Even or Odd
+function even_or_odd(number) {
+  let parity;
+  number % 2 === 0 ? parity = 'Even' : parity = 'Odd';
+  return parity;
+}
