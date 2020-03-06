7f3d99c65b2282a6e36f37d0639d6d2dbcf061cc
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-06 14:11:16 -0700

    Completes FindNumbersWhichAreDivisibleBy givenNum

---
+++ codewarsKatas.js
@@ -0,0 +1,11 @@
+//Find numbers which are divisible by given number
+function divisibleBy(numbers, divisor){
+  var dividends = [];
+  numbers.forEach(function(number){ 
+    if(number % divisor === 0){
+    dividends.push(number)};
+    });
+  
+  return dividends
+}
+
