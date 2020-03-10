8caafca1d5f5fd6bfe7fe1a095fe1253eaa2e840
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-10 12:24:20 -0600

    Completes GrassHopper Grade book

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,3 +1,24 @@
+//Grasshopper - Grade book
+function getGrade(s1, s2, s3){
+  let average = (s1+s2+s3)/3
+  if (average >= 90){
+    return "A";
+  }
+  if (average >= 80){
+    return "B";
+  }
+  if (average >= 70){
+    return "C";
+  }
+  if (average >= 60){
+    return "D";
+  }
+ return "F";
+}
+
+
+
+
 //Find numbers which are divisible by given number
 function divisibleBy(numbers, divisor){
   var dividends = [];
