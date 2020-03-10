b81bfc08653e4164ce5585b31d6d20cb750e394e
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-10 12:51:14 -0600

    Completes Beginner Sum of Numbers

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,3 +1,15 @@
+//Beginner Series #3 Sum of Numbers
+function GetSum( a,b ) {
+  let sum = 0;
+  let c;
+  a > b ? ( c = a, a = b, b = c) : (a = a , b = b)
+  for( let i = a ; i <= b ; ++i) {
+  sum += i
+  }
+  
+  return sum;
+}
+
 
 //Sum of two lowest positive integers
 function sumTwoSmallestNumbers(numbers) {  
