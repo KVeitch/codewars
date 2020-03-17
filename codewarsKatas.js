2ca184f13f0f8ffac44eeca285c9d5a9465a510f
Kirk Veitch <kirk.a.veitch@gmail.com>
2020-03-17 13:00:35 -0600

    Completes Rock Paper Scissors!

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,5 +1,19 @@
-//Century From Year
+//Rock Papaer Scissors!
+const rps = (p1, p2) => {
+  if(p1 === p2){
+    return 'Draw!';
+  } else if((p1 === 'scissors' && p2 === 'paper') || 
+            (p1 === 'rock' && p2 === 'scissors') || 
+            (p1 === 'paper' && p2 === 'rock')) {
+    return 'Player 1 won!'
+  }
+  return 'Player 2 won!'
+};
 
+//Century From Year
+function century(year) {
+  return Math.ceil(year / 100);
+}
 
 //Calculate Average
 // function find_average(array) {
