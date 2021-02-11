ef51b811b5f7ce4135bbdfcc75634ced21deb522
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-02-10 21:16:13 -0700

    Completes Move 10

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,5 +1,40 @@
 //Snail
 //Move 10
+function moveTen(s) {
+  return s
+    .split('')
+    .map((l) => charMap[l])
+    .join('');
+}
+
+const charMap = {
+  a: 'k',
+  b: 'l',
+  c: 'm',
+  d: 'n',
+  e: 'o',
+  f: 'p',
+  g: 'q',
+  h: 'r',
+  i: 's',
+  j: 't',
+  k: 'u',
+  l: 'v',
+  m: 'w',
+  n: 'x',
+  o: 'y',
+  p: 'z',
+  q: 'a',
+  r: 'b',
+  s: 'c',
+  t: 'd',
+  u: 'e',
+  v: 'f',
+  w: 'g',
+  x: 'h',
+  y: 'i',
+  z: 'j',
+};
 
 //Pair of Bears
 function bears(x, s) {
