82ccaad732b8ac71c5d3b4433e87a81d1c53af66
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-02-12 00:09:37 -0700

    Refactor Who Likes It

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,30 +1,40 @@
 //Snail
 //Who Likes It?
-function likes(names) {
-  names = names || [];
-  switch (names.length) {
-    case 0:
-      return 'no one likes this';
-      break;
-    case 1:
-      return names[0] + ' likes this';
-      break;
-    case 2:
-      return names[0] + ' and ' + names[1] + ' like this';
-      break;
-    case 3:
-      return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
-      break;
-    default:
-      return (
-        names[0] +
-        ', ' +
-        names[1] +
-        ' and ' +
-        (names.length - 2) +
-        ' others like this'
-      );
-  }
+// function likes(names) {
+//   names = names || [];
+//   switch (names.length) {
+//     case 0:
+//       return 'no one likes this';
+//       break;
+//     case 1:
+//       return names[0] + ' likes this';
+//       break;
+//     case 2:
+//       return names[0] + ' and ' + names[1] + ' like this';
+//       break;
+//     case 3:
+//       return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this';
+//       break;
+//     default:
+//       return (
+//         names[0] +
+//         ', ' +
+//         names[1] +
+//         ' and ' +
+//         (names.length - 2) +
+//         ' others like this'
+//       );
+//   }
+// }
+//refactor
+function likes (names) {
+  return {
+    0 : 'no one likes this',
+    1 : `${names[0]} likes this`,
+    2 : `${names[0]} and ${names[1]} like this`,
+    3 : `${names[0]}, ${names[1]} and ${names[2]} like this`,
+    4 : `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+  }[Math.min(4, names.length)]
 }
 //Two to One
 function longest(s1, s2) {
