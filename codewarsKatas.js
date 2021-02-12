9ebb363cb6173a325427981e208fa11c8ac05305
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-02-11 23:58:10 -0700

    Completes Two to One

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,5 +1,10 @@
 //Snail
 //Two to One
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2))
+    .sort()
+    .join('');
+}
 
 //Rearange Number to Get its Maximum
 const maxRedigit = (num) =>
