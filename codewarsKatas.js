485dde749c724617c0f4f7f33766147a21833163
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-02-13 22:49:38 -0700

    Completes Thinking & Testing : Uniq or not Uniq

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -36,7 +36,9 @@ const testit = (s) =>
     )
     .join(' ');
 //Thinking & Testing : Uniq or not Uniq
-
+function testit(a, b) {
+  return [...new Set(a), ...new Set(b)].sort((a, b) => a - b);
+}
 //Who Likes It?
 // function likes(names) {
 //   names = names || [];
