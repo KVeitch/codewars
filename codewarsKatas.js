7f1bbfe37702c3e0f58c968319a4c5af573a2396
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-02-23 23:58:29 -0700

    Completes Odd or Even?

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,5 +1,9 @@
 //Snail
 //Odd or Even?
+function oddOrEven(array) {
+  const sum = array.reduce((a, b) => a + b, 0);
+  return sum % 2 === 0 ? 'even' : 'odd';
+}
 //Sums of Parts
 function partsSums(ls) {
   let i = ls.length;
