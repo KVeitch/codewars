88c64d70c8b26290d8290d57d617b279cae144b0
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-03-02 13:35:31 -0700

    Completes Write Number in Expanded Form

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,6 +1,17 @@
 //Snail
 //Write Number in Expanded Form
+function expandedForm(num) {
+  let numStr = num.toString().split('');
 
+  for (let i = 0; i < numStr.length; i++) {
+    for (let y = numStr.length - i; y > 1; y--) {
+      numStr[i] += '0';
+    }
+  }
+
+  numStr = numStr.filter((value) => !value.startsWith(0));
+  return numStr.join(' + ');
+}
 
 //Braking well
 function dist(v, mu) {
