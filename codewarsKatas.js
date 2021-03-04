43519adb3c227637fc40e9ed3c569212fad6b334
Kirk Veitch <kirk.a.veitch@gmail.com>
2021-03-04 00:18:52 -0700

    Add phoneticMap to to_nato

--- codewarsKatas.js
+++ codewarsKatas.js
@@ -1,6 +1,40 @@
 //Snail
 //If you can read this...
+function to_nato(words) {
+  console.log(words.split(' ').map((str) => str.charAt(0)));
+}
 
+const phoneticMap = {
+  A: 'Alpha',
+  B: 'Bravo',
+  C: 'Charlie',
+  D: 'Delta',
+  E: 'Echo',
+  F: 'Foxtrot',
+  G: 'Golf',
+  H: 'Hotel',
+  I: 'India',
+  J: 'Juliett',
+  K: 'Kilo',
+  L: 'Lima',
+  M: 'Mike',
+  N: 'November',
+  O: 'Oscar',
+  P: 'Papa',
+  Q: 'Quebec',
+  R: 'Romeo',
+  S: 'Sierra',
+  T: 'Tango',
+  U: 'Uniform',
+  V: 'Victor',
+  W: 'Whiskey',
+  X: 'X-ray',
+  Y: 'Yankee',
+  Z: 'Zulu',
+  '.': '.',
+  '!': '!',
+  '?': '?',
+};
 //number with 3 roots.
 // function perfectRoots(n) {
 //   return (
